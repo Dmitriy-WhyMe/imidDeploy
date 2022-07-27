a5c19191820bf12bb9f43d74af723f335cd92ec2
Dmitriy <dima.ivahnoff@mail.ru>
2022-07-27 09:33:46 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,20 @@
 import Layout from "../components/global/Layout/Layout"
 import { useState } from 'react'
+/*
 import { 
     Start, SystemSelection, About, Benefits, Products, Questions, Support, News, Gallery, Contacts
 } from './IndexImport'
+*/
+import Start from '../components/pages/main/Start/Start'
+import SystemSelection from '../components/pages/main/SystemSelection/SystemSelection'
+import About from '../components/pages/main/About/About'
+import Benefits from '../components/pages/main/Benefits/Benefits'
+import Products from '../components/pages/main/Products/Products'
+import Questions from '../components/pages/main/Questions/Questions'
+import Support from '../components/pages/main/Support/Support'
+import News from '../components/pages/main/News/News'
+import Gallery from '../components/pages/main/Gallery/Gallery'
+import Contacts from '../components/pages/main/Contacts/Contacts'
 
 function Page(props) {
     const [content, setContent] = useState(props)
